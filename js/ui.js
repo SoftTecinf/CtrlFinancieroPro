d9a1573662875e485ffcf66d33fd169fb656c040
SoftTecinf <elenatpial@gmail.com>
2026-07-08 13:54:34 -0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -2,7 +2,6 @@
 function actualizarListadoIndividual(tipo, contId, countId) {
     const filtrados = obtenerMovimientosFiltrados().filter(m => m.tipo === tipo).reverse();
     document.getElementById(countId).innerText = `${filtrados.length} items`;
-   
     const cont = document.getElementById(contId);
     cont.innerHTML = filtrados.length ? '' : '<p class="opacity-20 text-center py-10">Sin registros.</p>';
 
@@ -150,11 +149,4 @@ function inicializarFiltros() {
             }
         });
     });
-}
-
-function formatCurrency(input, hiddenId) {
-    let value = input.value.replace(/\D/g, "");
-    let numericValue = value ? parseFloat(value) / 100 : 0;
-    document.getElementById(hiddenId).value = numericValue;
-    input.value = numericValue.toLocaleString('es-MX', { style: 'currency', currency: 'MXN' }) + " MXN";
 }
