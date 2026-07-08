0d3bf39872518956f4a833098c8d2979e9f06f73
SoftTecinf <elenatpial@gmail.com>
2026-07-08 13:27:39 -0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -6,7 +6,7 @@ function actualizarListadoIndividual(tipo, contId, countId) {
     const cont = document.getElementById(contId);
     //cont.innerHTML = filtrados.length ? '' : '<p class="opacity-20 text-center py-10">Sin registros.</p>';
     const filtrados = movimientos.filter(m => m.tipo === tipo).reverse();
-    
+
     filtrados.forEach(m => {
         cont.innerHTML += `
                     <div class="p-4 bg-gray-50/50 rounded-xl border border-white flex justify-between items-center group transition-all hover:bg-white hover:shadow-sm">
@@ -118,11 +118,13 @@ function actualizarFechaHeader() {
 }
 
 function toggleLoading(show) {
-    const loader = document.getElementById('loading-overlay');
+   /* const loader = document.getElementById('loading-overlay');
     // Solo intenta cambiar el estilo si el elemento existe en el HTML actual
     if (loader) {
         loader.style.display = show ? 'flex' : 'none';
-    }
+    }*/
+
+        console.log("Cargando:", estado);
 }
 
 function inicializarFiltros() {
