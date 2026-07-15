f7e7bbaa11743384cb20e8816b9d6176aa4155f1
SoftTecinf <elenatpial@gmail.com>
2026-07-15 14:03:23 -0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -157,8 +157,6 @@ function actualizarHome() {
         }
         window.datosGrafico.nuevosIngresos = ingM;
         window.datosGrafico.nuevosGastos = gasM;
-
-        console.log("Datos enviados al gráfico:", ingM, gasM);
     });
 
     // 3. ACTUALIZACIÓN DE TEXTOS (DOM)
