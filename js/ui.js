f916799356a98ec8adec3069a532d61b1d11a98b
SoftTecinf <elenatpial@gmail.com>
2026-07-16 14:50:11 -0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -177,13 +177,11 @@ function actualizarHome() {
         const ultimosMovs = [...datos].reverse().slice(0, 10);
         let nuevoHtml = '';
         ultimosMovs.forEach(m => {
-            console.log("Descripción encontrada:", m.desc);
-            
             nuevoHtml += `
                 <div class="flex justify-between items-center p-3 bg-gray-50/50 rounded-xl border border-white">
                     <div>
-                        <p>${m.desc}</p>
-                        <p class="text-[8px] opacity-40 uppercase">${window.formatearFechaMX(m.fecha)}</p>
+                        <p class="text-xs font-semibold uppercase">${m.desc}</p>
+                        <p class="text-[8px] opacity-40 uppercase">${m.fecha}</p>
                     </div>
                     <span class="text-xs font-bold ${m.tipo === 'gasto' ? 'text-rose-400' : 'text-stone-600'}">
                         ${m.tipo === 'gasto' ? '-' : '+'}${fMXN(m.monto)}
