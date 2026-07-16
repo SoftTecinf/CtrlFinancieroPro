f67f1dd32e2ac3f6c65e2851f916611a46a8e854
SoftTecinf <elenatpial@gmail.com>
2026-07-16 14:53:02 -0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -181,7 +181,7 @@ function actualizarHome() {
                 <div class="flex justify-between items-center p-3 bg-gray-50/50 rounded-xl border border-white">
                     <div>
                         <p class="text-xs font-semibold uppercase">${m.desc}</p>
-                        <p class="text-[8px] opacity-40 uppercase">${m.fecha}</p>
+                        <p class="text-[8px] opacity-40 uppercase">${window.formatearFechaMX(m.fecha)} || ${m.cat}</p>
                     </div>
                     <span class="text-xs font-bold ${m.tipo === 'gasto' ? 'text-rose-400' : 'text-stone-600'}">
                         ${m.tipo === 'gasto' ? '-' : '+'}${fMXN(m.monto)}
