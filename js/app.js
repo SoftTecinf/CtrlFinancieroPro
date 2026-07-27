c7ff39c888c53a7a0b2f65db4d60873cde942343
SoftTecinf <elenatpial@gmail.com>
2026-07-27 13:38:07 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // --- CONFIGURACIÓN Y ESTADO GLOBAL ---
-const API_URL = "https://script.google.com/macros/s/AKfycbw7BCRmlIEhrRb_xkj57BlDi-JvAxHU94PQe8FykPsSv0LcFM9yOQpSBAxZ0Xg2hKMI/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwRDYP0H0vHWFS5ifehCVAIxoeNV28OgnV8BbzCozgSmuOOZCCU5qn9nut9538k7V4g/exec";
 
 let editandoId = null;
 let chartH, chartR;
